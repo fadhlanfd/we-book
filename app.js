74ff74d8779d395545235e15a60f82dcee153bf9
Fadhlan Fadhlillah <fadhlan.fadlilah2000@gmail.com>
2022-08-03 20:24:36 +0700

    first commit

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
@@ -16,20 +16,43 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+app.use(session({
+  secret: "Our Little secret.",
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB");
 
+const postSchema = new mongoose.Schema({
+  judul: String,
+  isbn: String,
+  penerbit: String,
+  deskripsi: String
+});
+
+const Post = mongoose.model("Post", postSchema);
+
 const userSchema = new mongoose.Schema({
-  email: {type: String, required: true, unique: true},
-  password: {type: String, required: true}
+  user: String,
+  password: String,
+  profile: String,
+  address: String
 },
   {collections: "users"}
 );
 
-// Secret String Instead of Two Keys
-// Exclude Certain Fields from Encryption
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
   res.render("home");
@@ -43,48 +66,79 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
-app.post("/register", function(req, res){
-  const newUser = new User({
-    email : req.body.username,
-    password : req.body.password
-  });
-
-  newUser.save(function(err){
-      if (!err) {
-        res.render("secrets");
-      } else {
-        console.log(err);
-      }
-  });
-
+app.get("/books", function(req, res){
+  Post.find({}, function(err,posts){
+    res.render("books", {posts: posts})
+  })
 });
 
-app.post("/login", function(req, res){
-  const username = req.body.username;
-  const password = req.body.password;
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
 
-  User.findOne({email: username}, function(err, foundUser){
+app.post("/compose", function(req,res){
+    const post = new Post({
+      judul: req.body.postJudul,
+      isbn: req.body.postIsbn,
+      penerbit: req.body.postPenerbit,
+      deskripsi: req.body.postDeskripsi
+    });
+
+    post.save(function(err){
+      if(!err) {
+        res.redirect("/books");
+      }
+    });
+});
+
+app.get("/utama", function(req, res){
+  if (req.isAuthenticated()){
+    res.render("utama");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function(req, res){
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+app.post("/register", function(req, res){
+
+  User.register({username: req.body.username, profile: req.body.profile, address: req.body.address}, req.body.password, function(err, user){
     if (err) {
       console.log(err);
+      res.redirect("/register")
     } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
-      }
+      passport.authenticate("local")(req,res, function(){
+        res.redirect("/utama");
+      });
     }
   });
 
 });
 
+app.post("/login", function(req, res){
 
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
 
+  req.login(user, function(err){
+    if (err){
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req,res, function(){
+        res.redirect("/utama");
+      });
+    }
+  });
 
-
-
-
-
-
+});
 
 
 app.listen(3000, function() {
